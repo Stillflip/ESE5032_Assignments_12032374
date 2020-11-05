28bf5748e469cd4222cafce22c353999c25d59c2
stillfliped <13397945666@163.com>
2020-11-05 11:27:48 +0800

    PS3_5

--- assignment3/PS3_5.R
+++ assignment3/PS3_5.R
@@ -1,13 +1,13 @@
+library(dplyr)
 data5 <- read.csv('PS3.5_data.csv')
 data5_tbl <- as_tibble(data5)
 
-data_5_new <- data5_tbl %>%
-  mutate(dis=Distance, vec=abs(Velocity))
-model5 <- lm(dis ~ vec, data = data_5_new)
+model5 <- lm(Distance ~ Velocity, data = data5_tbl)
 coef(model5)
 
+coef(model5)[2]
 #5.1
-plot(dis ~ vec, data = data_5_new)
+plot(Distance ~ Velocity, data = data5_tbl)
 
 #5.2
 abline(model5, lwd = 3, col = "red")   # draw the regression line
