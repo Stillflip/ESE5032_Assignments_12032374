79a833c70ac72560c34b94c37127c48d21881f89
still <13397945666@163.com>
2020-11-19 16:30:22 +0800

    dd

--- assignment4/PS4_1.R
+++ assignment4/PS4_1.R
@@ -1,2 +1,62 @@
-RH <- read.csv('A_RH_VT-20130101-20161231的副本.csv')
-RH_tbl <- as_tibble(RH)
+TEMP <- read.csv('A_TEMP_R.csv')
+#temp_X1 <- TEMP$X1
+#temp_X1[which(temp_X1 == -99999)] <- NA
+
+TEMP_tbl <- as_tibble(TEMP)
+
+x1_x2 <- TEMP_tbl %>%
+  select(Time,X1,X2)%>%
+  filter(X1!=-99999,X2!=-99999) %>%
+  mutate(year_month=substr(Time,1,7),year= substr(Time,1,4),month=substr(Time,6,7),day = )%>% 
+  mutate(month_new= ifelse(substr(Time,7,7)!='/',month,paste(0,substr(month,1,1),sep = ''))) %>%
+  mutate(year_month_new = paste(year,month_new,sep = ''))%>%
+  group_by(year_month_new)%>%
+  summarise(X1,X2,X1_month_mean=mean(X1),X2_month_mean = mean(X2),Time) # ѡȡ??ƽ??
+
+
+#boxplot
+ggplot(x1_x2,aes(x=year_month_new,y=X1 ,fill=year_month_new)) +
+  geom_boxplot() +
+  theme_classic()
+
+x1_x2_uni <-unique( x1_x2 %>%
+  select(year_month_new,X1_month_mean,X2_month_mean))
+
+#Time series
+x1_time <- ts(x1_x2_uni$X1_month_mean, start=c(2014,1), frequency=12)
+plot(x1_time, type="l",col='blue', main='the month mean temp of X1 station',xlab = 'Time',
+     ylab = 'month mean temp') 
+
+temp_components <- decompose(x1_time)
+plot(temp_components)
+
+#Histogram
+hist(x1_x2_uni$X1_month_mean,     
+     xlab = "Monthly temp of station X1",
+     main = "Histogram of Monthly temp",
+     breaks = 20,
+     col = "blue",
+     border = "red")
+box(lwd=2,col="green")
+
+#Scatterplots
+
+X1_temp <- TEMP_tbl %>%
+  select(Time,X1)%>%
+  filter(X1 != -99999)
+
+  
+  
+plot(X1 ~ as.Date(Time), data = X1_temp,
+     xlab = "year_month",
+     ylab = "temp",
+     main = "monthly temp of station X1",
+     pch = "+",
+     cex = 2,
+     col = "navy")
+
+# image plot
+
+
+
+
--- assignment4/PS4_2.R
+++ assignment4/PS4_2.R
@@ -1,3 +1,9 @@
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(forecast)
+library(lubridate)
+
 baoan <- read.csv('2281305.csv')
 baoan_tbl <- as_tibble(baoan)
 tem_time <- baoan_tbl %>%
@@ -7,23 +13,105 @@ tem_time <- baoan_tbl %>%
   summarize(temp_mean=mean(as.numeric(temp_new), na.rm = TRUE))
 
 temp_year <- ts(tem_time$temp_mean, start=c(2010,1), frequency=12)
-plot(temp_year, type="l")
+plot(temp_year, type="l", col='red')
 
 #2.2
 temp_components <- decompose(temp_year)
-plot(temp_components)
+plot(temp_components,col='blue')
 
-hist(temp_components$random, prob=TRUE)
+hist(temp_components$random, prob=TRUE,col = "blue",border = "yellow")
 # Add pdf
 curve(dnorm(x, mean=mean(temp_components$random,na.rm=T),
             sd=sd(temp_components$random,na.rm=T)),
       add=TRUE, col="red")
 
 
-#AR Model
-par(mfrow=c(2,1))
-plot(arima.sim(list(order=c(1,0,0), ar=.9), n=100), ylab="x",
-     main=(expression(AR(1)~~~phi==+.9)))
-plot(arima.sim(list(order=c(1,0,0), ar=-.9), n=100), ylab="x",
-     main=(expression(AR(1)~~~phi==-.9)))
+#2.3 AR Model
+# we can see that the data has trend, so it is not a Stationary time series
+#so ,we need to do the difference operation
+
+#因为没有2020年10月份的真实数据，所以取2010年1月份-2019年4月份的数据进行拟合，
+#来预测2020年5月至9月的数据并使用真实值进行评估
+temp_for_fit <- ts(tem_time$temp_mean[1:124], start=c(2010,1), frequency=12)
+plot(temp_for_fit)
+
+temp_year_log = log(temp_for_fit)
+plot(temp_year_log,col='blue')
+
+# Check acf and pacf
+acf(temp_year_log)
+pacf(temp_year_log)
+
+# Take the diff, d=1
+temp_year_log_d1 <- diff(temp_year_log)
+
+# Plot time series
+plot(temp_year_log_d1,col='blue')
+
+# Check acf and pacf
+acf(temp_year_log_d1,col='blue')
+pacf(temp_year_log_d1,col='blue')
+
+# Automated forecasting using an ARIMA model
+model <- auto.arima(temp_year_log)
+summary(model)
+
+#2.4 predict
+# Make predictions 
+forecast_5months <- forecast(model, 5)
+plot(forecast_5months)
+
+
+# Plot predictions along with real values
+plot(forecast(model, 5), include = 30, xlab="Time", 
+     ylab="log(temp_year_log_d1)",type="o",lwd=2) 
+
+# Get predicted values
+
+# get the value of the latter 5 months
+day_forward <- 5
+predict_mean_values <- exp(forecast_5months$mean[day_forward])
+predict_values <- exp(forecast_5months$mean)
+exp(forecast_5months$mean[day_forward])
+exp(forecast_5months$lower[day_forward,1])
+exp(forecast_5months$upper[day_forward,1])
+
+
+
+# Verify the predictions
+real_values <- tail(tem_time)
+mean_values <- mean(real_values$temp_mean[2:6])
+bias = (mean_values - predict_mean_values)/mean_values
+print(bias)
+
+
+bias_every <- (real_values$temp_mean[2:6] - predict_values)/real_values$temp_mean[2:6]
+
+#mean_bias : 0.000226408
+#每个月份的误差
+# May          Jun          Jul          Aug          Sep
+# 2020  0.042162860  0.020763919  0.011700228 -0.020018560  0.007128324
+
+# predict values
+# forecast_5months
+#          Point Forecast    Lo 80    Hi 80    Lo 95    Hi 95
+# May 2020       5.585117 5.505395 5.664839 5.463193 5.707041
+# Jun 2020       5.660167 5.577796 5.742539 5.534191 5.786144
+# Jul 2020       5.703099 5.619204 5.786993 5.574793 5.831404
+# Aug 2020       5.700424 5.616530 5.784319 5.572118 5.828730
+# Sep 2020       5.678195 5.594301 5.762090 5.549890 5.806501
+
+#         May      Jun      Jul      Aug      Sep
+# 2020 266.4315 287.1967 299.7949 298.9942 292.4212
+
+# real_value
+
+# 1 2020-04         218.
+# 2 2020-05         278.
+# 3 2020-06         293.
+# 4 2020-07         303.
+# 5 2020-08         293.
+# 6 2020-09         295.
+
+
 
